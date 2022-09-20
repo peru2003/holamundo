e91c58418890a8fe28693e2cb0eddc0a0d1ddf93
peru2003 <110685516+peru2003@users.noreply.github.com>
2022-09-20 16:55:53 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hola mundo!");
+        private final static duble PI = 3.14;
+        private static int numero1;
+        public static void main (string [] args ){
+            scanner input = new scanner (system.in);
+            sistem.out. println ("digite primer numero ");
+            numero1= imput.nextInt();
+            system.out.println ("resultado: " + ( numero1 * PI));
+        }
+    }
+
